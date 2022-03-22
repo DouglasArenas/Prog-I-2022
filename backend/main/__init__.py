17e092a2af797530fc6e088bc4079e607d90182c
DouglasArenas <douglasarenas71@gmail.com>
2022-03-22 03:05:35 -0300

    agregando sqlalchemy

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 import main.resources as resources
 api = Api()
+db = SQLAlchemy()
 
 #Método que inicializará todos los módulos y devolverá la aplicación
 def create_app():
@@ -11,6 +13,11 @@ def create_app():
     app = Flask(__name__)
     #Cargar variables de entorno
     load_dotenv()
+    if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
+        os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+    db.init_app(app)
     #
     #Aquí se inicializarán el resto de los módulos de la aplicación
     api.add_resource(resources.QualificationResource, '/calificacion/<id>')
