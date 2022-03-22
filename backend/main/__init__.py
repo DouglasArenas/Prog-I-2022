e81dbf647fbfaf3fea6360b8c85098f50a8e2fd9
DouglasArenas <douglasarenas71@gmail.com>
2022-03-22 18:59:25 -0300

    init main

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,3 +1,16 @@
-!#bin/bash
+import os
+from flask import Flask
+from dotenv import load_dotenv
 
-#from .. import ..
+#Método que inicializará todos los módulos y devolverá la aplicación
+def create_app():
+    #Inicializar Flask
+    app = Flask(__name__)
+    #Cargar variables de entorno
+    load_dotenv()
+    #
+    #Aquí se inicializarán el resto de los módulos de la aplicación
+    #
+    #Retornar aplicación inicializada
+    return app
+
