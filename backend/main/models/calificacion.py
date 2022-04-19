25968478c452fe8aa9d5ec78e988c1a14aa4c49e
DouglasArenas <douglasarenas71@gmail.com>
2022-04-19 19:37:49 -0300

    fin de clase

--- backend/main/models/calificacion.py
+++ backend/main/models/calificacion.py
@@ -1,30 +1,31 @@
 from .. import db
+from . import UserModel
 
 class Qualification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.Integer, nullable=True)
-    userId = db.Column(db.Integer)
-    poemId = db.Column(db.Integer)
+    userId = db.Column(db.Integer, db.ForeingKey('user.id'), nullable=False)
+    poemId = db.Column(db.Integer, db.ForeingKey('poem.id'), nullnable=False)
+    author = db.relationship('User', back_populates="qualification", uselist=False, single_parents=True)
 
     def __repr__(self):
         return f'<Score: {self.score}, Comment: {self.comment}, User: {self.userId}, Poem {self.poemId}>'
 
     def to_json(self):
+        self.author = db.session.query(UserModel).get_or_404(self.userId)
         qualification_json = {
             'id' : self.id,
             'score' : self.score,
             'comment' : self.comment,
-            'userId' : self.userId,
-            'poemId' : self.poemId
+            'author' : self.author.to_json()
         }
         return qualification_json
 
     def to_json_short(self):
         qualification_json = {
             'id' : self.id,
-            'score' : self.score,
-            'userId' : self.userId
+            'score' : self.score
         }
         return qualification_json
 
--- backend/main/models/poema.py
+++ backend/main/models/poema.py
@@ -3,9 +3,10 @@ from .. import db
 class Poem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeingKey('user.id'), nullable=False)
     body = db.Column(db.String(1000), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
+    author = relatio
 
     def __repr__(self):
         return f'<Title: {self.title}, UserId: {self.user_id}, Poem: {self.body}, Date {self.date}>'
