236106ff0d0956d37f68497db2d39d894d95b857
Douglas <douglasarenas71@gmail.com>
2022-05-02 13:55:45 -0300

    paginacion en curso

--- backend/main/resources/poem.py
+++ backend/main/resources/poem.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource
 from flask import request, jsonify
 from .. import db
-from main.models import PoemModel
+from main.models import PoemModel, UserModel
+import datetime
+from sqlalchemy import func
 
 class Poem(Resource):
     def get(self, id):
@@ -14,10 +16,59 @@ class Poem(Resource):
         db.session.commit()
         return '', 204
 
+    #def put(self, id):
+        poem = db.session.query(PoemModel).get_or_404(id)
+        data = request.get_json().items()
+        for key, value in data:
+            setattr(poem,key,value)
+        db.session.add(poem)
+        db.session.commit()
+        return poem.to_json(), 201
+
 class Poems(Resource):
     def get(self):
-        poems = db.session.query(PoemModel).all()
-        return jsonify([poem.to_json_short() for poem in poems])
+        poems = db.session.query(PoemModel)
+        page = 1
+        per_page = 10
+        if request.get_json():
+            filters = request.get_json().items()
+            for key, value in filters:
+                # Paginate
+                if key == "page":
+                    page = int(value)
+                if key == "per_page":
+                    per_page = int(value)
+                if key == "title":
+                    poems = poems.filter(PoemModel.title.like('%'+value+'%'))
+                if key == "user_id":
+                    poems = poems.filter(PoemModel.user_id == value)
+                if key == "user_name":
+                    poems = poems.filter(PoemModel.user_name.has(UserModel.user_name.like('%'+value+'%')))
+                if key == "created[gt]":
+                    poems = poems.filter(PoemModel.date_time >= datetime.strptime(value, '%d-%m-%Y'))
+                if key == "created[lt]":
+                    poems = poems.filter(PoemModel.date_time <= datetime.strptime(value, '%d-%m-%Y'))
+                # Order
+                if key == "sort_by":
+                    if value == "author":
+                        poems = poems.order_by(PoemModel.author)
+                    if value == "author[desc]":
+                        poems = poems.order_by(PoemModel.author.desc())
+                    if value == "date":
+                        poems == poems.order_by(PoemModel.date)
+                    if value == "date[desc]":
+                        poems = poems.order_by(PoemModel.date.desc())
+                    if value == "qualification":
+                        poems = poems.outerjoin(PoemModel.qualifications).group_by(PoemModel.id).order_by(func.avg(PoemModel.score))
+                    if value == "qualification[desc]":
+                        poems = poems.outerjoin(PoemModel.qualifications).group_by(PoemModel.id).order_by(func.avg(PoemModel.score).desc())
+        poems = poems.paginate(page, per_page, False, 30)
+        return jsonify({
+            "poems" : [poem.to_json_short() for poem in poems.items],
+            "total" : poems.total,
+            "pages" : poems.pages,
+            "page" : page
+            })
 
     def post(self):
         poem = PoemModel.from_json(request.get_json())
--- backend/main/resources/user.py
+++ backend/main/resources/user.py
@@ -2,6 +2,7 @@ from flask_restful import Resource
 from flask import request, jsonify
 from .. import db
 from main.models import UserModel
+from sqlalchemy import func
 
 class User(Resource):
     def get(self, id):
@@ -25,8 +26,35 @@ class User(Resource):
 
 class Users(Resource):
     def get(self):
-        users = db.session.query(UserModel).all()
-        return jsonify([user.to_json_short() for user in users])
+        page = 1
+        per_page = 10
+        users = db.session.query(UserModel)
+        if request.get_json():
+            filters = request.get_json().items()
+            for key, value in filters:
+                if key == "page":
+                    page = int(value)
+                if key == "per_page":
+                    per_page = int(value)
+                if key == "name":
+                    users = users.filter(UserModel.name.like('%'+value+'%'))
+                # Order
+                if key == "sort_by":
+                    if key == 'name':
+                        users = users.order_by(UserModel.name.like('%'+value+'%'))
+                    if value == "npoems[desc]":
+                        users = users.order_by(func.count(UserModel.id).desc())
+                    if value == "num_poems":
+                        users = users.outerjoin(UserModel.poems).group_by(UserModel.id).order_by(func.count(UserModel.id))
+                    if value == "num_qualifications":
+                        users = users.outerjoin(UserModel.qualifications).group_by(UserModel.id).order_by(func.count(UserModel.id))
+        users = users.paginate(page, per_page, False, 30)
+        return jsonify({
+            'user' : [user.to_json_short() for user in users.items],
+            'total' : users.total,
+            'pages' : users.pages,
+            'page' : page
+        })
 
     def post(self):
         user = UserModel.from_json(request.get_json())
