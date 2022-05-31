005b515e4b2f449a0a04a6c7b555ed35e45cccfa
DouglasArenas <douglasarenas71@gmail.com>
2022-05-31 16:17:31 -0300

    mail no send

--- backend/main/resources/user.py
+++ backend/main/resources/user.py
@@ -3,28 +3,24 @@ from flask import request, jsonify
 from .. import db
 from main.models import UserModel, UserModel
 from sqlalchemy import func
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from main.auth.decorators import admin_required
 
 class User(Resource):
-    @jwt_required()
+    @jwt_required(optional=True)
     def get(self, id):
         user_id = get_jwt_identity()
-        poem = PoemModel.from_json(request.get_json())
+        user = UserModel.from_json(request.get_json())
         user = db.session.query(UserModel).get_or_404(user_id)
         claims = get_jwt()
         if "role" in claims:
-            if claims["role"] == "poet":
-                if len(user.poems) == 0 or len(user.qualifications) >= 2:
-                    poem.user_id = user_id
-                    db.session.add(poem)
-                    db.session.commit()
-                    return poem.to_json(), 201
-                else:
-                    return "There are not enough qualifications from this user"
+            if claims["role"] == "admin":
+                user.user_id = user_id
+                db.session.add(user)
+                db.session.commit()
+                return user.to_json(), 201
             else:
-                return "Only poets can create poems"
-        return user.to_json()
+                return user.to_json_short(), 201
     
     @jwt_required()
     def put(self, id):
@@ -63,10 +59,10 @@ class Users(Resource):
                 if key == "sort_by":
                     if key == 'name':
                         users = users.order_by(UserModel.name)
-                    if value == "num_poems[desc]":
-                        users = users.outerjoin(UserModel.poems).group_by(UserModel.id).order_by(func.count(UserModel.id).desc())
-                    if value == "num_poems":
-                        users = users.outerjoin(UserModel.poems).group_by(UserModel.id).order_by(func.count(UserModel.id))
+                    if value == "num_users[desc]":
+                        users = users.outerjoin(UserModel.users).group_by(UserModel.id).order_by(func.count(UserModel.id).desc())
+                    if value == "num_users":
+                        users = users.outerjoin(UserModel.users).group_by(UserModel.id).order_by(func.count(UserModel.id))
                     if value == "num_qualifications":
                         users = users.outerjoin(UserModel.qualifications).group_by(UserModel.id).order_by(func.count(UserModel.id).desc())
         users = users.paginate(page, per_page, True, 30)
